8b5e5b01deb866f0e4ac2cecf0f81a1b131c2764
Guilherme <91488762+Gui148@users.noreply.github.com>
2023-07-25 16:37:54 -0300

    Resolved Error

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,16 @@ import React from 'react';
 
 function Header() {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7b530c81f3a588d2065a1e3be1821ac8f1173c4f
     <div className="flex justify-between bg-gray-800 text-white w-full h-14 items-center p-6 fixed z-50">
       <a className="text-2xl max-md:text-lg font-bold">TRABALHO</a>
-=======
-    <div className="flex justify-between bg-gray-800 text-white w-full h-14 items-center p-6 fixed">
-      <a className="text-2xl max-md:text-lg">TRABALHO</a>
->>>>>>> parent of 45c8e46 (app)
       <div className="space-x-4 max-md:space-x-2 max-md:text-sm ">
-        <a href="#infografico" className=" hover:text-gray-400">InfoGrafico</a>
-        <a href="#minivideo" className=" hover:text-gray-400">Video</a>
-        <a href="#podcast" className=" hover:text-gray-400">PodCast</a>
+        <a href="#infografico" className="hover:text-gray-400">InfoGrafico</a>
+        <a href="#minivideo" className="hover:text-gray-400">Video</a>
+        <a href="#podcast" className="hover:text-gray-400">PodCast</a>
       </div>
     </div>
   );
 }
 
 export default Header;
+
