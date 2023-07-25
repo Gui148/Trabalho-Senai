b2a28c0e3b7e8971cfdcd8fe782fb74deab41b38
Guilherme <91488762+Gui148@users.noreply.github.com>
2023-07-24 22:35:59 -0300

    Header Video

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
       <div className="space-x-4 max-md:space-x-2 max-md:text-sm ">
         <a href="#infografico" className=" hover:text-gray-400">InfoGrafico</a>
         <a href="#podcast" className=" hover:text-gray-400">PodCast</a>
-        <a href="#minivideo" className=" hover:text-gray-400">MiniVideo</a>
+        <a href="#minivideo" className=" hover:text-gray-400">Video</a>
       </div>
     </div>
   );
