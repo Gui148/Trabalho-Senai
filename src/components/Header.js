d720da11cf9fe8951251f4157d9ea320e2cd75f0
Guilherme <91488762+Gui148@users.noreply.github.com>
2023-07-24 22:34:02 -0300

    Header @media

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Header() {
   return (
     <div className="flex justify-between bg-gray-800 text-white w-full h-14 items-center p-6">
-      <a className="text-2xl">TRABALHO</a>
-      <div className="space-x-4">
+      <a className="text-2xl max-md:text-lg">TRABALHO</a>
+      <div className="space-x-4 max-md:space-x-2 max-md:text-sm ">
         <a href="#infografico" className=" hover:text-gray-400">InfoGrafico</a>
         <a href="#podcast" className=" hover:text-gray-400">PodCast</a>
         <a href="#minivideo" className=" hover:text-gray-400">MiniVideo</a>
