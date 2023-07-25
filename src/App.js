87e26c115fc88e13154cb6f1f54959153e1d5e79
Guilherme <91488762+Gui148@users.noreply.github.com>
2023-07-25 16:11:54 -0300

    dadwawdaw

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="bg-[#253140] text-white p-6">
       <div id="infografico" className="flex justify-center flex-col items-center">
         <h1 className="font-bold text-center text-3xl mb-5 max-md:text-xl">INFOGRAFICO</h1>
-        <img src={InfoGrafico} className="w-[900px] h-full" alt="InfoGrafico" title="InfoGrafico"/>
+        <img src={InfoGrafico} className="w-[900px]  h-full" alt="InfoGrafico" title="InfoGrafico"/>
         <hr className=""/>
       </div>
       <div id="minivideo" className=" "></div>
