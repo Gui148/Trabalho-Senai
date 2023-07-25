65ef27cc460109aa9adbdcd5d2855468d073f26c
Guilherme <91488762+Gui148@users.noreply.github.com>
2023-07-24 22:38:26 -0300

    App @media

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ function App() {
   return (
     <div className="bg-[#253140] text-white p-6">
       <div id="infografico" className="flex justify-center flex-col items-center">
-        <h1 className="font-bold text-center text-3xl mb-5">INFOGRAFICO</h1>
+        <h1 className="font-bold text-center text-3xl mb-5 max-md:text-xl">INFOGRAFICO</h1>
         <img src={InfoGrafico} className="w-[900px] h-full" alt="InfoGrafico" title="InfoGrafico"/>
         <hr className=""/>
       </div>
       <div id="minivideo" className="">
-        <h2 className="font-bold text-center text-3xl mb-5 mt-5">VIDEO</h2>
+        <h2 className="font-bold text-center text-3xl mb-5 mt-5 max-md:text-xl">VIDEO</h2>
         <video src={Video} width={1280} height={720} controls>
         </video>
+      </div>
+      <div id="podcast" className="">
       </div>
     </div>
   );
