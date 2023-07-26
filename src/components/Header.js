38804dcadd013956a26dfbe8c6e160ff47d5b655
Guilherme <91488762+Gui148@users.noreply.github.com>
2023-07-26 15:06:30 -0300

    Uptade Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ function Header() {
         <a href="#infografico" className="hover:text-gray-400">InfoGrafico</a>
         <a href="#minivideo" className="hover:text-gray-400">Video</a>
         <a href="#podcast" className="hover:text-gray-400">PodCast</a>
+        <a href="#podcast" className="hover:text-gray-400">Soft Skills</a>
       </div>
     </div>
   );
